595447fa316a8883951f07dc8dd6e4d72199c158
Adam Taylor <adam.taylor@sagebase.org>
2021-10-13 17:34:34 +0100

    use argparse

--- make_contactsheet.py
+++ make_contactsheet.py
@@ -1,3 +1,5 @@
+
+import argparse
 from tifffile import TiffFile
 from matplotlib.image import imsave
 from matplotlib.pyplot import imshow
@@ -7,6 +9,28 @@ import numpy as np
 import sys
 import os
 
+
+parser = argparse.ArgumentParser(description = 'Create a contact sheet of scenes and channels from a tiff')
+parser.add_argument(
+    'input',
+    type=str,
+    help='the input file (openable by tifffile)'
+    )
+parser.add_argument(
+    '--output',
+    destination = "output"
+    type=str,
+    default = "contactsheet.png"
+    help='the output file name'
+    )
+parser.add_argument('--dpi',
+    dest = "dpi",
+    type=int,
+    default = 600,
+    help='the output dpi'
+    )
+
+args = parser.parse_args()
 
 def pull_series(path, level):
     with TiffFile(path) as tif:
@@ -20,7 +44,7 @@ def plot_rgb(images):
     fig, axs = plt.subplots(len(images))
     for index, image in enumerate(images):
         channel_index = np.argmin(image.shape)  
-        if channel_index == 3:
+        if image.shape[channel_index] == 3:
             axs[index].imshow(image)
             axs[index].axis('off')
         else: 
@@ -32,15 +56,13 @@ def plot_rgb(images):
     return(fig)
 
 def main():
-    path = sys.argv[1]
-    output = 'contactsheet.png'
 
-    images = pull_series(path, -1)
+    images = pull_series(args.input, -1)
     fig  = plot_rgb(images)
 
     fig
 
-    plt.savefig(output, dpi = 300, bbox_inches='tight')
+    plt.savefig(args.output, dpi = args.dpi, bbox_inches='tight')
 
 if __name__ == "__main__":
     main()  
